6ed6717127ab6228a6a9bfa51a0ff29457ea8551
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-17 19:16:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,7 +22,7 @@ def assign_rooms(array)
     array.each do |name|
     new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
   counter += 1
-  end
+end
   return new_array
 end
   
